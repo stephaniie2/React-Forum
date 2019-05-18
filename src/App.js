24470d1607201a653026465793ee9426a9240390
Stephanie <stephaniiehallberg@gmail.com>
2019-05-18 14:18:52 +0200

    learn redux tools

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 
-import Navbar from './components/Navbar';
-import Channels from './components/Channels';
-import AppRouter from './components/AppRouter';
+import Navbar from "./components/Navbar";
+import Channels from "./components/Channels";
+import AppRouter from "./components/AppRouter";
 
 class App extends Component {
   render() {
-    const isNotAuthPath = this.props.history.location.pathname !== '/register' && this.props.history.location.pathname !== '/login';
+    const isNotAuthPath =
+      this.props.history.location.pathname !== "/register" &&
+      this.props.history.location.pathname !== "/login";
     return (
       <Fragment>
         <Navbar />
         <div className="my-4">
           <div className="container">
             <div className="row justify-content-center">
-              {
-                 isNotAuthPath &&
+              {isNotAuthPath && (
                 <div className="col-md-4">
-                  <button className="btn btn-info my-3 form-control">Start thread</button>
+                  <button className="btn btn-info my-3 form-control">
+                    Start thread
+                  </button>
                   <Channels />
                 </div>
-              }
+              )}
               <div className="col-md-8">
                 <AppRouter />
               </div>
