db6580366cdaa1689c0f40c082ae0062725a9d14
Stephanie <stephaniiehallberg@gmail.com>
2019-05-18 14:04:40 +0200

    initial commit1

---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import RegisterForm from "./RegisterForm";
+
+class RegisterContainer extends Component {
+  render() {
+    return <RegisterForm />;
+  }
+}
+
+export default RegisterContainer;
